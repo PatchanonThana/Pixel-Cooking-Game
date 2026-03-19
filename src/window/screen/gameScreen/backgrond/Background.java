b2c872f387b3ff5844c1c16349f77bef576b50ac
PatchanonThana <shogon42@gmail.com>
2026-03-19 19:19:00 +0700

    refactor(background): change menuScreen, gameScreen background

--- src/window/screen/gameScreen/backgrond/Background.java
+++ src/window/screen/gameScreen/backgrond/Background.java
@@ -4,19 +4,35 @@ import window.screen.gameScreen.GameScreenListener;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class Background extends JPanel implements GameScreenListener {
+
+    final private Image backgroundImg;
+    private Dimension thisSize;
+
     public  Background() {
-        setBackground(Color.BLACK);
+        backgroundImg = new ImageIcon(Objects.requireNonNull(getClass().getResource(
+                "/window/screen/screenImage/Mainscreen.png"))
+        ).getImage();
     }
 
     @Override
     public void gameScreenResized(Dimension size) {
+
+        thisSize = new Dimension(size.width,size.height);
+
         setBounds(
           0,
           0,
           size.width,
           size.height
         );
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImg,0,0,thisSize.width,thisSize.height,this);
     }
 }
