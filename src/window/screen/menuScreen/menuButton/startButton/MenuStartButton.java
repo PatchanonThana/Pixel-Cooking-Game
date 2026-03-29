f9af3ba911e0d9bb8f52fb2de6ce37b96da28d45
raindewywy <suwanchindachayapa@gmail.com>
2026-03-29 20:44:56 +0700

    MenuScreen
    menu logo start quit back

--- src/window/screen/menuScreen/menuButton/startButton/MenuStartButton.java
+++ src/window/screen/menuScreen/menuButton/startButton/MenuStartButton.java
@@ -6,56 +6,69 @@ import window.screen.menuScreen.MenuListener;
 import javax.swing.*;
 import java.awt.*;
 import java.util.Objects;
-import javax.sound.sampled.*;
 
 import static java.awt.Cursor.HAND_CURSOR;
 
-
 public class MenuStartButton extends JButton implements MenuListener {
 
-    final private Dimension thisSize = new Dimension(300,100);
-    MenuStartButtonListener startButtonListener;
+    private final Dimension thisSize = new Dimension(600, 150);
+    private final MenuStartButtonListener startButtonListener;
 
     public MenuStartButton(MenuStartButtonListener startButtonListener){
         this.startButtonListener = startButtonListener;
+
         setCursor(Cursor.getPredefinedCursor(HAND_CURSOR));
 
+        // --------- Img ---------
         ImageIcon rawStart = new ImageIcon(Objects.requireNonNull(getClass().getResource(
                 "/window/screen/menuScreen/menuButton/startButton/startImage/startbutton.png"
-                        )));
-        Image scaledStart = rawStart.getImage().getScaledInstance(thisSize.width, thisSize.height, Image.SCALE_SMOOTH);
-        Icon startImage = new ImageIcon(scaledStart);
-        setIcon(startImage);
-
-        ImageIcon rawHoverStart = new ImageIcon(
-                Objects.requireNonNull(getClass().getResource(
-                    "/window/screen/menuScreen/menuButton/startButton/startImage/Startbutton-hover.png"
-                ))
+        )));
+        Image scaledStart = rawStart.getImage().getScaledInstance(
+                thisSize.width, thisSize.height, Image.SCALE_SMOOTH
         );
-        Image scaledHoverStart = rawHoverStart.getImage().getScaledInstance(thisSize.width,thisSize.height,Image.SCALE_SMOOTH);
-        Icon hoverStart = new ImageIcon(scaledHoverStart);
-        setRolloverIcon(hoverStart);
+        setIcon(new ImageIcon(scaledStart));
 
+        // --------- Hover ---------
+        ImageIcon rawHoverStart = new ImageIcon(Objects.requireNonNull(getClass().getResource(
+                "/window/screen/menuScreen/menuButton/startButton/startImage/startbutton-hover.png"
+        )));
+        Image scaledHoverStart = rawHoverStart.getImage().getScaledInstance(
+                thisSize.width, thisSize.height, Image.SCALE_SMOOTH
+        );
+        setRolloverIcon(new ImageIcon(scaledHoverStart));
 
+        // --------- FIX ---------
+        setMargin(new Insets(0, 0, 0, 0));
+        setBorder(null);
+        setRolloverEnabled(true);
+
+        setSize(thisSize);
+        setPreferredSize(thisSize);
+        setMinimumSize(thisSize);
+        setMaximumSize(thisSize);
+
+        setHorizontalAlignment(SwingConstants.CENTER);
+        setVerticalAlignment(SwingConstants.CENTER);
+
+        // --------- UI ---------
         setFocusPainted(false);
-        setBorderPainted(false);
         setContentAreaFilled(false);
         setOpaque(false);
 
+        // --------- Sound ---------
         ButtonSoundPlayer buttonSoundPlayer = new ButtonSoundPlayer();
 
         addActionListener(e -> {
-                    buttonSoundPlayer.playSound();
-                    this.startButtonListener.menuStartButtonClicked();
-                }
-        );
+            buttonSoundPlayer.playSound();
+            this.startButtonListener.menuStartButtonClicked();
+        });
     }
 
     @Override
     public void menuResized(Dimension size) {
         setBounds(
-                (int) (size.getWidth()/2 - thisSize.width/2.0),
-                (int) (size.getHeight()/2 - thisSize.height/2.0) ,
+                (int)(size.getWidth()/2 - thisSize.width/2.0),
+                (int)(size.getHeight()/2 - thisSize.height/2.0),
                 thisSize.width,
                 thisSize.height
         );
