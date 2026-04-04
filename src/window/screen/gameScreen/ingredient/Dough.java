e7e5d531837c9d164a9cd7b059a108d13a807bea
Nat <karn1122549@gmail.com>
2026-04-04 15:58:17 +0700

    เพิ่มคลิกขวา

--- src/window/screen/gameScreen/ingredient/Dough.java
+++ src/window/screen/gameScreen/ingredient/Dough.java
@@ -1,4 +1,32 @@
 package window.screen.gameScreen.ingredient;
+import window.screen.gameScreen.CutBoard.CutBoard;
+import window.screen.gameScreen.ingredient.Ingredient;
+import window.screen.gameScreen.pot.Pot;
 
-public class Dough {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Dough extends Ingredient{
+    public Dough(int startX , int startY , Pot pot, CutBoard cutBoard){
+        super("/dessert/dough.png", 950 , 700,15,9,pot,cutBoard);
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e){
+                if(SwingUtilities.isRightMouseButton(e)){
+                    Container parent = getParent();
+                    Point currentLocation = getLocation();
+
+                    parent.remove(Dough.this);
+
+                    circleDough circledough = new circleDough(currentLocation.x,currentLocation.y,pot,cutBoard);
+                    parent.add(circledough, JLayeredPane.DRAG_LAYER);
+                    parent.revalidate();
+                    parent.repaint();
+                }
+            }
+        });
+    }
 }
