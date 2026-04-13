11b2b304c57d1f77f13f92ef3abfcb50f027fb99
Nat <karn1122549@gmail.com>
2026-04-13 22:09:00 +0700

    Revert "add recipe and cutBoard function"
    This reverts commit 0c2405010c4a66f460e28de55acd49b4651d24c0.

--- src/window/screen/gameScreen/ingredient/Ingredient.java
+++ src/window/screen/gameScreen/ingredient/Ingredient.java
@@ -15,6 +15,18 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class Ingredient extends JComponent implements GameScreenListener {
+    //add
+    public enum Type {
+        WATER,
+        OIL,
+        FOOD,
+        OTHER
+    }protected Type type = Type.OTHER;
+    public Type getType() {
+        return type;
+    }
+
+
     private String filename;
     private double relX , relY; //พิกัดจริง
     private int startX, startY;
@@ -55,14 +67,28 @@ public class Ingredient extends JComponent implements GameScreenListener {
         this.addMouseListener(clickListener);
         this.addMouseMotionListener(dragListener);
     }
+    //add บอกว่ามันคืออะไร --> ตั้งว่า ingredient นี้เป็น WATER,OIL,FOOD
+    public Ingredient(String filename, double relX, double relY, double relWidth, double relHeight, Pot pot, Type type) {
 
-    //constructor สำหรับวัตถุดิบที่ต้องใช้เขียง
+        this(filename, relX, relY, relWidth, relHeight, pot);
+        this.type = type;
+    }
+
+    //constructor สำหรับวัตถุดิบที่ต้องใช้เขียง --> วัตถุดิบนี้ใช้กับเขียงได้
     public Ingredient(String filename, double relX,double relY, double relWidth, double relHeight, Pot pot , CutBoard cutBoard) {
         this(filename , relX , relY , relWidth , relHeight , pot);
         this.cutBoard = cutBoard;
         this.isVisible = true;
         setOpaque(false);
 
+    }
+
+    public String getFilename(){
+        return filename;
+    }
+
+    public void returnToStart(){
+        setLocation(startX,startY);
     }
 
 
@@ -82,6 +108,10 @@ public class Ingredient extends JComponent implements GameScreenListener {
 
             boolean onPot = (pot != null) && pot.getPotZone().intersects(ingredientRect);
             boolean onBoard = (cutBoard != null) && cutBoard.getBoardZone().intersects(ingredientRect);
+            //add --> ส่งวัตถุดิบนี้เข้าไปในหม้อ ใน Pot เช็ค เป็น WATER → เปลี่ยนเป็นหม้อน้ำ, เป็น OIL → เปลี่ยนเป็นหม้อน้ำมัน
+            if (onPot) {
+                pot.addIngredient(Ingredient.this);
+            }
             if (!onPot && !onBoard){
                 setLocation(startX,startY);
             }
@@ -109,10 +139,10 @@ public class Ingredient extends JComponent implements GameScreenListener {
             int newY = parentPoint.y - pressedPoint.y;
             setLocation(newX, newY);
         }
-
-
     }
-    //เพิ่มวัตถุบน gamescreen
+
+
+    //เพิ่มวัตถุบน gamescreen 2
     @Override
     public void gameScreenResized(Dimension size){
         startX = (int)(size.width * relX);
@@ -122,7 +152,7 @@ public class Ingredient extends JComponent implements GameScreenListener {
         setBounds(startX , startY , width,height);
     }
 
-    //วาดวัตถุเริ่มต้น
+    //วาดวัตถุเริ่มต้น 1
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
